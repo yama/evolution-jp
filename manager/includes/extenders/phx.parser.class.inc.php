2dee18322effeb291e906489bfdb66912dd36942
yamamoto <yamamoto@kyms.jp>
2015-10-04 15:33:28 +0900

    New - maxモディファイア・minモディファイア

--- manager/includes/extenders/phx.parser.class.inc.php
+++ manager/includes/extenders/phx.parser.class.inc.php
@@ -452,6 +452,10 @@ class PHx {
                 if(!$opt) $opt = 0;
                 $value = $cmd($value,$opt);
                 break;
+            case 'max':
+            case 'min':
+                $value = $cmd(explode(',',$value));
+                break;
 			case 'addbreak':
 				$value = $this->addbreak($value);
 				break;
