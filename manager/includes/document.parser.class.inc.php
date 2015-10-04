64eebcf936f1a383ee8bb3c593bde0234cb3ca73
yamamoto <yamamoto@kyms.jp>
2015-10-04 14:38:34 +0900

    Fix - テンプレートのネスト

--- manager/includes/document.parser.class.inc.php
+++ manager/includes/document.parser.class.inc.php
@@ -464,7 +464,20 @@ class DocumentParser {
                             {
                                 if(substr($parent->content,0,5)==='@FILE')
                                     $parent->content = $this->atBindFile($parent->content);
-                                $template->content = str_replace('[*content*]', $template->content, $parent->content);
+                                elseif(substr($template->content,0,4)==='@URL')
+                                    $parent->content = $this->atBindUrl($parent->content);
+                                if(strpos($parent->content,'[*content*]')!==false)
+                                    $template->content = str_replace('[*content*]', $template->content, $parent->content);
+                                elseif(strpos($parent->content,'[*content:')!==false)
+                                {
+                                    $matches = $this->getTagsFromContent($parent->content,'[*content:','*]');
+                                    if($matches[0])
+                                    {
+                                        $modifier = $matches[1][0];
+                                        $template->content = str_replace($matches[0][0], $template->content, $parent->content);
+                                        $template->content = str_replace('[*content*]',"[*content:{$modifier}*]",$template->content);
+                                    }
+                                }
                                 if(!empty($parent->parent)) $parent = $this->db->getObject('site_templates',"id='{$parent->parent}'");
                                 else break;
                             }
